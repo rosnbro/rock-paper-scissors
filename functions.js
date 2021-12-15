7a8bbc8113e6f967c38d081e03463945efe8fed6
rosnbro <benro95@gmail.com>
2021-12-14 21:16:11 -0500

    changed the game code to compare strings instead of integers

--- functions.js
+++ functions.js
@@ -1,51 +1,42 @@
 function computerSelection() {
-    const computer = Math.floor(Math.random() * 3);
-    return computer;
+    const compSelect = Math.floor(Math.random() * 3);            //selects a random integer from 0 to 2
+    let comp = "";
+
+    if (compSelect == 0) {
+        comp += "rock";              //converts the computer selected integer into a choice of rock, paper, or scissors
+    } else if (compSelect == 1) {
+        comp += "paper";
+    } else if (compSelect == 2) {
+        comp += "scissors"
+    }
+    return comp;
 }
 
 function playerSelection() {
-    let selection = window.prompt("Select one: Rock, Paper, or Scissors:");
-    let player = 0;
-
-    if (selection.toLowerCase === "rock") {
-        player = 0;
-    } else if (selection.toLowerCase === "paper") {
-        player = 1;
-    } else (selection.toLowerCase === "scissors"); {
-        player = 2;
-    }
-    return player;
+    let playSelect = window.prompt("Select one: Rock, Paper, or Scissors:");         //prompts user for their choice
+    let play = playSelect.toLowerCase();
+    return play;
 }
 
 function playRound() {
     let computer = computerSelection();
     let player = playerSelection();
-    let winnner = 0;
+    let winner = 0;
 
-    if (player == 0) {
-        if (computer == 0) {
-            winner = 0;
-        } else if (computer == 1) {
-            winner = 2;
-        } else if (computer == 2) {
-            winner = 1;
-        }
-    } else if (player == 1) {
-        if (computer == 0) {
-            winner = 1;
-        } else if (computer == 1) {
-            winner = 0;
-        } else if (computer == 2) {
-            winner = 2;
-        }
-    } else if (player == 2) {
-        if (computer == 0) {
-            winner = 2;
-        } else if (computer == 1) {
-            winner = 1;
-        } else if (computer == 2) {
-            winner = 0;
-        }
+    if ((player == "rock" && computer == "rock") || 
+        (player == "paper" && computer == "paper") ||
+        (player == "scissors" && computer == "scissors")) {            //compares the selections of the player and computer
+        winner = 0;
+    } else 
+    if ((player == "rock" && computer == "scissors") ||
+        (player == "paper" && computer == "rock") ||
+        (player == "scissors" && computer == "paper")) {
+        winner = 1;
+    } else 
+    if ((player == "rock" && computer == "paper") ||
+        (player == "paper" && computer == "scissors") ||
+        (player == "scissors" && computer == "rock")) {
+        winner = 2;
     }
     return winner;
 }
@@ -58,7 +49,7 @@ function game() {
         let roundWin = playRound();
 
         if (roundWin == 0) {
-            i--;
+            i--;                                        //keeps score of who wins each round, repeats in the case of a tie
             console.log("It's a tie.");
         } else if (roundWin == 1) {
             wins++;
@@ -68,10 +59,9 @@ function game() {
             console.log("You lost this round...")
         }
         if (wins == 3 || losses == 3) {
-            break;
+            break;                                      //ends the game after someone wins a best of five
         }
     }
-
     if (wins == 3) {
         return "Congratulations! You win!";
     } else {
