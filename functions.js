9a93d9a7eddf95a94d2f76638697ecac7f618e0b
rosnbro <benro95@gmail.com>
2022-01-06 20:42:22 -0500

    changed UI of game and altered page structure

--- functions.js
+++ functions.js
@@ -1,19 +1,38 @@
 let score = [0, 0];
-let roundWiner;
+let roundWinner;
 
+const buttons = document.querySelector('.buttons');
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
 const results = document.querySelector('.results');
-const roundWin = document.createElement('div');
+const roundResults = document.querySelector('.ticker');
+const playerScore = document.querySelector('.playerScore');
+const computerScore = document.querySelector('.computerScore');
+const roundWin = document.querySelector('.roundWin');
+const overlay = document.querySelector('.overlay');
 const gameWin = document.createElement('div');
 const restartPrompt = document.createElement('div');
 const restartButton = document.createElement('button');
+const instructions = document.createElement('div');
 
 rockButton.addEventListener('click', () => start("rock"));
 paperButton.addEventListener('click', () => start("paper"));
 scissorsButton.addEventListener('click', () => start("scissors"));
 restartButton.addEventListener('click', () => restart());
+overlay.addEventListener('click', () => {
+    overlay.style.display = "none"
+    overlay.firstElementChild.remove();
+});
+
+window.onload = intro();
+
+function intro() {
+    instructions.innerHTML = "<p>We are at war!</p><p>Enemy forces have sent one of their three specialized attack groups to destroy us, codenamed: Rock, Paper, and Scissors</p> <p>You must choose the correct unit to counter their attack- Paper beats Rock, Scissors beat Paper, & Rock beats Scissors</p><p>Securing vicotry in five battles should be enough to win the war.</p><p>Good luck.</p>";
+    instructions.classList.add("instructions");
+    overlay.appendChild(instructions);
+    overlay.style.display = "block";
+}
 
 
 function computerSelection() {
@@ -33,21 +52,22 @@ function playRound(player) {
     if ((player == "rock" && computer == "rock") || 
         (player == "paper" && computer == "paper") ||
         (player == "scissors" && computer == "scissors")) {            //compares the selections of the player and computer
-        roundWiner = "tie";
+        roundWinner = "Tie.";
     } else 
     if ((player == "rock" && computer == "scissors") ||
         (player == "paper" && computer == "rock") ||
         (player == "scissors" && computer == "paper")) {
-        roundWiner = "player";
+        roundWinner = "You won this round!";
         score[0]++;
     } else 
     if ((player == "rock" && computer == "paper") ||
         (player == "paper" && computer == "scissors") ||
         (player == "scissors" && computer == "rock")) {
-        roundWiner = "computer";
+        roundWinner = "You lost. Get it together.";
         score[1]++
     }
-    console.log(roundWiner);
+    roundWin.innerHTML = `${roundWinner} <br>Your opponent struck with ${computer}.`;
+    roundResults.insertBefore(roundWin, computerScore);
 }
 
 function start(player) {
@@ -55,29 +75,40 @@ function start(player) {
     if (score[0] >= 5 || score[1] >= 5) {
         end();
     }
-
+    playerScore.textContent = `${score[0]}`;
+    computerScore.textContent = `${score[1]}`;
 }
 
 function end() {
     if (score[0] == 5) {
-        gameWin.textContent = "You have emerged triumphant you glorious son of a gun.";
+        gameWin.textContent = "You have emerged triumphant, glorious victor!";
     } else {
-        gameWin.textContent = "Mission failed. We'll get 'em next time."
+        gameWin.innerHTML = "Mission failed. <br>We'll get 'em next time."
     }
     restartPrompt.classList.add("restartPrompt");
-    restartPrompt.textContent = "More hostiles incoming! Engage?";
     restartButton.classList.add("restartButton");
-    restartButton.textContent = "Oh yeah."
-    results.appendChild(gameWin);
+    gameWin.classList.add('gameWin');
+
+    restartPrompt.textContent = "More hostiles incoming! Engage?";
+    restartButton.textContent = "Oh yeah.";
+
+    overlay.appendChild(gameWin);
     results.appendChild(restartPrompt);
     results.appendChild(restartButton);
+
+    buttons.style.display = 'none';
+    overlay.style.display = "block";
 }
 
 function restart() {
     score = [0, 0];
+    playerScore.textContent = `${score[0]}`;
+    computerScore.textContent = `${score[1]}`;
+
     let children = results.firstElementChild;
     while (children) {
         children.remove();
         children = results.firstElementChild;
     }
+    buttons.style.display = 'flex';
 }
